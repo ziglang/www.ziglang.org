8c2bb855ee7b699b3defc76358624be0911b3cb1
Loris Cro <kappaloris@gmail.com>
2021-01-12 23:58:04 +0100

    launch

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-s3cmd sync -P --no-mime-magic --add-header="Cache-Control: max-age=0, must-revalidate" "public/" "s3://ziglang.org/temporary_website_test3/"
+s3cmd sync -P --no-mime-magic --add-header="Cache-Control: max-age=0, must-revalidate" "public/" "s3://ziglang.org/"
