6bcb187818596d6c5c3f2573b4e1f5c471f0f934
Andrew Kelley <andrew@ziglang.org>
2021-01-12 16:17:07 -0700

    deploy without s3cmd sync
    because it takes too long trying to be clever

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,4 +3,4 @@
 set -x
 set -e
 
-s3cmd sync -P --no-mime-magic --add-header="Cache-Control: max-age=0, must-revalidate" "public/" "s3://ziglang.org/"
+s3cmd put -P --no-mime-magic --recursive --add-header="Cache-Control: max-age=0, must-revalidate" "public/" "s3://ziglang.org/"
