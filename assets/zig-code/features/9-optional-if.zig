456c14355bdda4d17dc027526eb5a77ae771ac18
Loris Cro <kappaloris@gmail.com>
2021-03-12 20:58:39 +0100

    test execution of frontpage snippet and add names to other snippets

--- assets/zig-code/features/9-optional-if.zig
+++ assets/zig-code/features/9-optional-if.zig
@@ -3,7 +3,7 @@ fn doAThing(optional_foo: ?*Foo) void {
     // do some stuff
 
     if (optional_foo) |foo| {
-      doSomethingWithFoo(foo);
+        doSomethingWithFoo(foo);
     }
 
     // do some stuff
