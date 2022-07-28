0b9e8dac5df246453dbeb5718efb8f48b624ef30
Loris Cro <kappaloris@gmail.com>
2022-07-28 14:37:25 +0200

    update example to latest zig

--- assets/zig-code/features/13-errors-catch.zig
+++ assets/zig-code/features/13-errors-catch.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 
 pub fn main() void {
     const file = std.fs.cwd().openFile("does_not_exist/foo.txt", .{}) catch |err| label: {
-        std.debug.print("unable to open file: {e}\n", .{err});
+        std.debug.print("unable to open file: {}\n", .{err});
         const stderr = std.io.getStdErr();
         break :label stderr;
     };
