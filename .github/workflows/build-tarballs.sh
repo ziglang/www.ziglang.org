af9d69d5f284089d4c9a16acf4e926e9da5e90b1
Andrew Kelley <andrew@ziglang.org>
2023-01-17 16:02:23 -0700

    restore the sha256 checksums
    Partial revert of commit cabcc49e794a59889f0dc7633f03b21dd85f1ba4.
    Partial revert of commit 1164e10206975b3940362b58419a53c2149730a2.
    
    Additionally, sign index.json and place it at
    `https://ziglang.org/builds/zig-$VERSION-index.json.minisig`.

--- .github/workflows/build-tarballs.sh
+++ .github/workflows/build-tarballs.sh
@@ -288,46 +288,60 @@ s3cmd put -P --add-header="cache-control: public, max-age=31536000, immutable" z
 s3cmd put -P --add-header="cache-control: public, max-age=31536000, immutable" zig-windows-x86-$ZIG_VERSION.zip.minisig s3://ziglang.org/builds/
 
 export SRC_TARBALL="zig-$ZIG_VERSION.tar.xz"
+export SRC_SHASUM=$(sha256sum "zig-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)
 export SRC_BYTESIZE=$(wc -c < "zig-$ZIG_VERSION.tar.xz")
 
 export X86_64_LINUX_TARBALL="zig-linux-x86_64-$ZIG_VERSION.tar.xz"
 export X86_64_LINUX_BYTESIZE=$(wc -c < "zig-linux-x86_64-$ZIG_VERSION.tar.xz")
+export X86_64_LINUX_SHASUM="$(sha256sum "zig-linux-x86_64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export X86_64_MACOS_TARBALL="zig-macos-x86_64-$ZIG_VERSION.tar.xz"
 export X86_64_MACOS_BYTESIZE=$(wc -c < "zig-macos-x86_64-$ZIG_VERSION.tar.xz")
+export X86_64_MACOS_SHASUM="$(sha256sum "zig-macos-x86_64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export AARCH64_LINUX_TARBALL="zig-linux-aarch64-$ZIG_VERSION.tar.xz"
 export AARCH64_LINUX_BYTESIZE=$(wc -c < "zig-linux-aarch64-$ZIG_VERSION.tar.xz")
+export AARCH64_LINUX_SHASUM="$(sha256sum "zig-linux-aarch64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export AARCH64_MACOS_TARBALL="zig-macos-aarch64-$ZIG_VERSION.tar.xz"
 export AARCH64_MACOS_BYTESIZE=$(wc -c < "zig-macos-aarch64-$ZIG_VERSION.tar.xz")
+export AARCH64_MACOS_SHASUM="$(sha256sum "zig-macos-aarch64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 #export X86_64_FREEBSD_TARBALL="zig-freebsd-x86_64-$ZIG_VERSION.tar.xz"
 #export X86_64_FREEBSD_BYTESIZE=$(wc -c < "zig-freebsd-x86_64-$ZIG_VERSION.tar.xz")
+#export X86_64_FREEBSD_SHASUM="$(sha256sum "zig-freebsd-x86_64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 #export X86_64_NETBSD_TARBALL="zig-netbsd-x86_64-$ZIG_VERSION.tar.xz"
 #export X86_64_NETBSD_BYTESIZE=$(wc -c < "zig-netbsd-x86_64-$ZIG_VERSION.tar.xz")
+#export X86_64_NETBSD_SHASUM="$(sha256sum "zig-netbsd-x86_64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export RISCV64_LINUX_TARBALL="zig-linux-riscv64-$ZIG_VERSION.tar.xz"
 export RISCV64_LINUX_BYTESIZE=$(wc -c < "zig-linux-riscv64-$ZIG_VERSION.tar.xz")
+export RISCV64_LINUX_SHASUM="$(sha256sum "zig-linux-riscv64-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export POWERPC64LE_LINUX_TARBALL="zig-linux-powerpc64le-$ZIG_VERSION.tar.xz"
 export POWERPC64LE_LINUX_BYTESIZE=$(wc -c < "zig-linux-powerpc64le-$ZIG_VERSION.tar.xz")
+export POWERPC64LE_LINUX_SHASUM="$(sha256sum "zig-linux-powerpc64le-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export POWERPC_LINUX_TARBALL="zig-linux-powerpc-$ZIG_VERSION.tar.xz"
 export POWERPC_LINUX_BYTESIZE=$(wc -c < "zig-linux-powerpc-$ZIG_VERSION.tar.xz")
+export POWERPC_LINUX_SHASUM="$(sha256sum "zig-linux-powerpc-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export X86_LINUX_TARBALL="zig-linux-x86-$ZIG_VERSION.tar.xz"
 export X86_LINUX_BYTESIZE=$(wc -c < "zig-linux-x86-$ZIG_VERSION.tar.xz")
+export X86_LINUX_SHASUM="$(sha256sum "zig-linux-x86-$ZIG_VERSION.tar.xz" | cut '-d ' -f1)"
 
 export X86_64_WINDOWS_TARBALL="zig-windows-x86_64-$ZIG_VERSION.zip"
 export X86_64_WINDOWS_BYTESIZE=$(wc -c < "zig-windows-x86_64-$ZIG_VERSION.zip")
+export X86_64_WINDOWS_SHASUM="$(sha256sum "zig-windows-x86_64-$ZIG_VERSION.zip" | cut '-d ' -f1)"
 
 export AARCH64_WINDOWS_TARBALL="zig-windows-aarch64-$ZIG_VERSION.zip"
 export AARCH64_WINDOWS_BYTESIZE=$(wc -c < "zig-windows-aarch64-$ZIG_VERSION.zip")
+export AARCH64_WINDOWS_SHASUM="$(sha256sum "zig-windows-aarch64-$ZIG_VERSION.zip" | cut '-d ' -f1)"
 
 export X86_WINDOWS_TARBALL="zig-windows-x86-$ZIG_VERSION.zip"
 export X86_WINDOWS_BYTESIZE=$(wc -c < "zig-windows-x86-$ZIG_VERSION.zip")
+export X86_WINDOWS_SHASUM="$(sha256sum "zig-windows-x86-$ZIG_VERSION.zip" | cut '-d ' -f1)"
 
 export MASTER_DATE="$(date +%Y-%m-%d)"
 export MASTER_VERSION="$ZIG_VERSION"
@@ -339,8 +353,14 @@ cd "$WEBSITEDIR/.github/workflows"
 # this is a long-running process).
 git pull
 "$ZIG" run update-download-page.zig
-mv out/index.json "$WEBSITEDIR/data/releases.json"
 
+echo | minisign -Sm out/index.json
+s3cmd put -P \
+  --add-header="cache-control: public, max-age=31536000, immutable" \
+  out/index.json.minisig \
+  s3://ziglang.org/builds/zig-$ZIG_VERSION-index.json.minisig
+
+mv out/index.json "$WEBSITEDIR/data/releases.json"
 cd "$WEBSITEDIR"
 
 # This is the user when pushing to the website repo.
