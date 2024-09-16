1d9edaefe2816850992c589cdc04d8678ea2ed16
Sonic <sonic.from.new.yoke@gmail.com>
2024-09-16 16:07:48 +0300

    add markup of translation & translate index page

--- build.zig
+++ build.zig
@@ -147,6 +147,12 @@ pub fn build(b: *std.Build) void {
                 .site_title = "Zig Programming Language",
                 .content_dir_path = "content/it-IT",
             },
+            .{
+                .code = "uk-UA",
+                .name = "Українська",
+                .site_title = "Zig Programming Language",
+                .content_dir_path = "content/uk-UA",
+            },
         },
     });
 }
