8c442369b368ebeca21c8fe6845032a9da2dc036
Andrew Kelley <andrew@ziglang.org>
2025-03-02 18:32:16 -0800

    start working on upgrading site to zig 0.14.0

--- assets/zig-code/build-system/conditional-compilation/build.zig
+++ assets/zig-code/build-system/conditional-compilation/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "app",
         .root_source_file = b.path("app.zig"),
-        .target = b.host,
+        .target = b.graph.host,
     });
 
     const version = b.option([]const u8, "version", "application version string") orelse "0.0.0";
--- assets/zig-code/build-system/convenience-run-step/build.zig
+++ assets/zig-code/build-system/convenience-run-step/build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_source_file = b.path("hello.zig"),
-        .target = b.host,
+        .target = b.graph.host,
     });
 
     b.installArtifact(exe);
