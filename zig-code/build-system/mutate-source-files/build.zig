cb3fe1711b7e6c068da01a25aee412fcdd07d68e
Loris Cro <kristoff@Loriss-MacBook-Air.local>
2025-05-10 11:36:37 +0200

    update addUpdateSourceFiles example in learn/build-system
    closes #465

--- zig-code/build-system/mutate-source-files/build.zig
+++ zig-code/build-system/mutate-source-files/build.zig
@@ -1,21 +1,24 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
     const exe = b.addExecutable(.{
         .name = "demo",
         .root_source_file = b.path("src/main.zig"),
+        .target = target,
     });
     b.installArtifact(exe);
 
     const proto_gen = b.addExecutable(.{
         .name = "proto_gen",
         .root_source_file = b.path("tools/proto_gen.zig"),
+        .target = target,
     });
 
     const run = b.addRunArtifact(proto_gen);
     const generated_protocol_file = run.addOutputFileArg("protocol.zig");
 
-    const wf = b.addWriteFiles();
+    const wf = b.addUpdateSourceFiles();
     wf.addCopyFileToSource(generated_protocol_file, "src/protocol.zig");
 
     const update_protocol_step = b.step("update-protocol", "update src/protocol.zig to latest");
