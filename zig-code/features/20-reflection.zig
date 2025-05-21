efe9cd9e50585f5c6a420872e09bf1cab655c16d
Yifan Hu <141280278+b0ae989c@users.noreply.github.com>
2025-05-21 04:28:38 +0000

    fix typo in example code

--- zig-code/features/20-reflection.zig
+++ zig-code/features/20-reflection.zig
@@ -11,7 +11,7 @@ pub fn main() void {
 
 fn printInfoAboutStruct(comptime T: type) void {
     const info = @typeInfo(T);
-    inline for (info.@"struct".fields) |field| {
+    inline for (info.Struct.fields) |field| {
         std.debug.print(
             "{s} has a field called {s} with type {s}\n",
             .{
