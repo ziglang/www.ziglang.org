043697a601fd4714e0e8c3aefd567384ada84bae
Andrew Kelley <andrew@ziglang.org>
2025-08-20 17:33:48 -0700

    update build-commit script to 0.15.x

--- .github/workflows/build-commit.zig
+++ .github/workflows/build-commit.zig
@@ -445,7 +445,9 @@ fn zigVer(env_map: *const std.process.EnvMap, dir: std.fs.Dir) ![]const u8 {
 
             const ancestor_ver = try std.SemanticVersion.parse(tagged_ancestor);
             if (zig_version.order(ancestor_ver) != .gt) {
-                fatal("Zig version '{}' must be greater than tagged ancestor '{}'", .{ zig_version, ancestor_ver });
+                fatal("Zig version '{f}' must be greater than tagged ancestor '{f}'", .{
+                    zig_version, ancestor_ver,
+                });
             }
 
             // Check that the commit hash is prefixed with a 'g' (a Git convention).
@@ -513,16 +515,16 @@ fn allocPrintCmd(argv: []const []const u8) []u8 {
 }
 
 fn fetch(url: []const u8, headers: std.http.Client.Request.Headers, extra_headers: []const std.http.Header) ![]u8 {
-    var response: std.ArrayList(u8) = .init(arena);
+    var response: std.Io.Writer.Allocating = .init(arena);
     var client: std.http.Client = .{ .allocator = arena };
     const result = try client.fetch(.{
         .location = .{ .url = url },
-        .response_storage = .{ .dynamic = &response },
+        .response_writer = &response.writer,
         .headers = headers,
         .extra_headers = extra_headers,
     });
-    if (result.status != .ok) fatal("fetch from {s} result: {s}", .{ url, @tagName(result.status) });
-    return response.items;
+    if (result.status != .ok) fatal("fetch from {s} result: {t}", .{ url, result.status });
+    return response.written();
 }
 
 fn pluckLastSuccessFromJson(json_text: []const u8) []const u8 {
@@ -661,7 +663,7 @@ fn addTemplateEntry(
     const size = (try file.stat()).size;
     const digest = try sha256sum(file, size);
     try map.put(arena, print("{s}-tarball", .{name}), tarball_basename);
-    try map.put(arena, print("{s}-shasum", .{name}), print("{}", .{std.fmt.fmtSliceHexLower(&digest)}));
+    try map.put(arena, print("{s}-shasum", .{name}), print("{x}", .{&digest}));
     try map.put(arena, print("{s}-bytesize", .{name}), print("{d}", .{size}));
 }
 
@@ -703,7 +705,7 @@ fn render(
 ) !void {
     const in_contents = try std.fs.cwd().readFileAlloc(arena, in_file, 1 * 1024 * 1024);
 
-    var buffer = std.ArrayList(u8).init(arena);
+    var buffer: std.Io.Writer.Allocating = .init(arena);
     defer buffer.deinit();
 
     const State = enum {
@@ -712,7 +714,7 @@ fn render(
         VarName,
         EndBrace,
     };
-    const writer = buffer.writer();
+    const writer = &buffer.writer;
     var state = State.Start;
     var var_name_start: usize = undefined;
     var line: usize = 1;
@@ -742,7 +744,7 @@ fn render(
                         const trimmed = mem.trim(u8, value, " \r\n");
                         if (fmt == .html and mem.endsWith(u8, var_name, "bytesize")) {
                             const size = try std.fmt.parseInt(u64, trimmed, 10);
-                            try writer.print("{:.1}", .{std.fmt.fmtIntSizeDec(size)});
+                            try writer.print("{Bi:.1}", .{size});
                         } else {
                             try writer.writeAll(trimmed);
                         }
@@ -767,7 +769,7 @@ fn render(
             line += 1;
         }
     }
-    try out_dir.writeFile(.{ .sub_path = out_file, .data = buffer.items });
+    try out_dir.writeFile(.{ .sub_path = out_file, .data = buffer.written() });
 }
 
 fn updateWebsiteRepo(
