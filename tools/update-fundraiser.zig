b81eb69ae9cdc5a3c80de030405d9b016ba60710
Andrew Kelley <andrew@ziglang.org>
2025-09-21 00:54:46 -0700

    update fundraiser banner day 21

--- tools/update-fundraiser.zig
+++ tools/update-fundraiser.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
-const zero_raised = 3405;
-const ziggy_raised = 6863;
+const zero_raised = 3525;
+const ziggy_raised = 6973;
 const carmen_raised = 2139;
-const current_day = 19;
+const current_day = 21;
 const days = 30;
 const days_left: comptime_float = days - current_day;
 const max_raised: comptime_float = @max(zero_raised, ziggy_raised, carmen_raised);
